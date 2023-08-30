9a3bcdcd65391de6de69daa774e3d2bdb62f8de9
Bart King <bart@fireflylearning.com>
2023-08-30 14:27:01 +0100

    delete unnecessary security allowance

--- src/JsonDocumentPath/Properties/AssemblyInfo.cs
+++ src/JsonDocumentPath/Properties/AssemblyInfo.cs
@@ -1,12 +1,10 @@
 using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Security;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AllowPartiallyTrustedCallers]
 [assembly: InternalsVisibleTo("JsonDocumentPath.Test")]
 
 [assembly: AssemblyTrademark("")]
